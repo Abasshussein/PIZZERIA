5a13dde21a3752f181540c9ada7b64391b10dff0
Abasshussein <abasshussein03@gmail.com>
2020-03-01 21:44:37 +0300

    Added script.js file

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("submit").click(function (event) {
+        event.preventDefault();
+        var flavour = parseInt($("#flavours :selected").val())
+        var size = parseInt($("#sizes :selected").val())
+        var crust = parseInt($("#crusts :selected").val())
+        var toppings = parseInt($("#toppings :selected").val())
+        var number = parseInt($("#number").val())
+
+        var total = flavour + size + crust + toppings;
+        var grandTotal = total * number;
+
+        $(".deliver").click(function () {
+            var fields = prompt("Enter your address")
+            prompt("Enter your name")
+            prompt("Enter your location")
+            alert("Your order will delivered" + areas)
+            alert("Delivery cost is 200$")
+        });
+        $(".deliver").click(function () {
+            alert("Pick your order in our nearest shop")
+        });
+    })
+})
