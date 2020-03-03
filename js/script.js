76196b26c1834aa3a39fe946a52ce098d50e605e
Abasshussein <abasshussein03@gmail.com>
2020-03-03 16:17:22 +0300

    Made changes on script.js file hence changes its functioning

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var top = parseInt(document.getElementById("break").value);
 var num = parseInt(document.getElementById("undo").value);
 var totalAmount = num*(tas + s + c + top);
 alert(" The Total Amount is kshs "  +  totalAmount  +  ""  +  " Thank you for your order welcome again.");
+prompt("Enter your name")
 prompt("enter your location")
 alert("You delivery fee is 150/=")
 alert("You order will be delivered shortly")
